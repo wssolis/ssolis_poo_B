5abd37b762af2a06f6280bc3f10bf4b26f1cb2a4
wssolis <sys.solis@gmail.com>
2022-07-04 22:08:05 -0500

    Este es el proyecto base de la materia de POO v1.0

---
+++ src/ec/edu/intsuperior/vista/Aplicacion.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ec.edu.intsuperior.vista;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Santiago Solis
+ */
+public class Aplicacion {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null,"Pilas estudie sino ya no venga a clases"
+                + "y no se este quejando con el pana coordinador");
+        JOptionPane.showMessageDialog(null,JOptionPane.showInputDialog("Ingresa tu nombre"));
+    }
+    
+}
